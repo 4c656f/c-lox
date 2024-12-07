e7efeff4ac925ee77439508bbf90265cef1b09d3
4c656f <vlevo.yovlyo@yandex.ru>
2024-12-07 23:08:22 +0700

    feature: working chunk array, vm interp

---
+++ src/chunk.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "chunk.h"
+#include "memory.h"
+#include "value.h"
+
+void initChunk(Chunk *chunk) {
+  chunk->capacity = 0;
+  chunk->count = 0;
+  chunk->code = NULL;
+  chunk->lines = NULL;
+  initValueArray(&chunk->constants);
+}
+
+void writeChunk(Chunk *chunk, uint8_t byte, int line) {
+  // need to grow
+  if (chunk->capacity <= chunk->count) {
+    int oldCap = chunk->capacity;
+    chunk->capacity = GROW_CAPACITY(oldCap);
+    chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCap, chunk->capacity);
+    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCap, chunk->capacity);
+  }
+
+  chunk->code[chunk->count] = byte;
+  chunk->lines[chunk->count] = line;
+  chunk->count++;
+}
+
+void freeChunk(Chunk *chunk) {
+  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  FREE_ARRAY(int, chunk->lines, chunk->capacity);
+  freeValueArray(&chunk->constants);
+  initChunk(chunk);
+}
+
+int addConstant(Chunk *chunk, Value value) {
+  writeValueArray(&chunk->constants, value);
+
+
+  return chunk->constants.count - 1;
+}
---
+++ src/chunk.h
@@ -0,0 +1,30 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+  OP_RETURN,
+  OP_CONSTANT,
+  OP_NEGATE,
+  OP_ADD,
+  OP_SUBTRACT,
+  OP_MULT,
+  OP_DIVIDE,
+} OpCode;
+
+typedef struct {
+  int count;
+  int capacity;
+  uint8_t *code;
+  ValueArray constants;
+  int *lines;
+} Chunk;
+
+void initChunk(Chunk *chunk);
+void freeChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
+int addConstant(Chunk *chunk, Value value);
+
+#endif
